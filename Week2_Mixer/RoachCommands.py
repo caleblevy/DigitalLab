9ee42ca3a0c5af34028ab6e28e93df09cc653c1c
Caleb Levy <clevy@cepheid.ugastro.berkeley.edu>
2014-02-24 20:50:26 -0800

    Added roach commands, to be completed

---
+++ Week2_Mixer/RoachCommands.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+import os
+os.system("ls")
+os.chdir('/boffiles')
+os.system("ls -l")
+PID = os.system(
--- Week2_Roach/Roach_Data.py
+++ Week2_Roach/Roach_Data.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.fft import *
+from subprocess import call
+
+a = np.fromfile('abc_bram',count=-1,sep=" ")
 
